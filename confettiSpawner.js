1ca6f297dede8676260a3c054e8c9ec7b8c924c1
Julian Müller <julian.mueller@stud-mail.uni-wuerzburg.de>
2019-12-21 22:58:04 +0100

    add default values for get parameters

--- confettiSpawner.js
+++ confettiSpawner.js
@@ -2,6 +2,7 @@
 const params = new URLSearchParams(window.location.search);
 if (params.has("confetti")) confetti(parseInt(params.get("confetti")));
 if (params.has("text")) confetti.text = params.get("text");
+if (!params.has("confetti")&&!params.has("text")) window.location.href = window.location + "?confetti=1000&text=Welcome"
 
 // spawn via click
 document.addEventListener("click", ev => {
